febe80c4fc2bae91ae26ffa6d4b342371e614c83
Ryan Denby <ryan.543@live.co.uk>
2021-11-15 16:17:58 +0000

    Update

--- init.el
+++ init.el
@@ -1451,8 +1451,8 @@
 (use-package multiple-cursors
   :straight t
   :bind (("C-S-c C-S-c" . mc/edit-lines)
-         ("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-previous-like-this)
+         ("C-<" . mc/mark-next-like-this)
+         ("C->" . mc/mark-previous-like-this)
          ("C-c m a" . mc/mark-all-like-this)))
 
 (defun copy-word ()
