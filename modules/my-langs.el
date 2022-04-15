2eedd05688814c5c0679b0efc91c2c6288dfe656
Ryan Denby <ryan.543@live.co.uk>
2022-04-15 23:42:53 +0100

    Override backends for elisp

--- modules/my-langs.el
+++ modules/my-langs.el
@@ -104,7 +104,10 @@
 
 (use-package emacs-lisp-mode
   :straight (emacs-lisp-mode :type built-in)
-  :hook (lisp-mode . emacs-lisp-mode))
+  :hook (lisp-mode . emacs-lisp-mode)
+  :init
+  (set-company-backend! 'emacs-lisp-mode
+    '(company-elisp :with company-yasnippet company-files)))
 
 (use-package scheme-mode
   :straight (scheme-mode :type built-in)
