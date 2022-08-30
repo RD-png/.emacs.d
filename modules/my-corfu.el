2babaf7326e03527d7dbed402b13aecff5928674
Ryan Denby <ryan.543@live.co.uk>
2022-08-31 00:31:32 +0100

    Kind icon

--- modules/my-corfu.el
+++ modules/my-corfu.el
@@ -35,4 +35,12 @@
   :init
   (global-corfu-mode))
 
+(use-package kind-icon
+  :straight t
+  :after corfu
+  :custom
+  (kind-icon-default-face 'corfu-default)
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
 (provide 'my-corfu)
