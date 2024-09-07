1ed04b4807fc6ec8e78647db0495c04c1d610e3e
Ryan Denby <ryan.543@live.co.uk>
2024-09-07 01:03:36 +0100

    Trying activities over tabspaces

--- modules/my-tabspaces.el
+++ modules/my-tabspaces.el
@@ -17,6 +17,7 @@
 
 (use-package tabspaces
   :straight t
+  :disabled t
   :hook (after-init . tabspaces-mode)
   :bind (("C-c s s" . tabspaces-switch-or-create-workspace)
          ("C-c s k" . tabspaces-close-workspace)
@@ -34,5 +35,58 @@
   (tabspaces-include-buffers '("*scratch*" "*Messages*"))
   (tabspaces-session t)
   (tabspaces-session-auto-restore t))
+
+(use-package activities
+  :straight t
+  :bind (("C-c s c" . activities-new)
+         ("C-c s s" . activities-switch)
+         ("C-c s k" . activities-kill)
+         ("C-c s l" . activities-list)
+         ("C-c s r" . activities-rename)
+         ("C-c s o" . activities-resume)
+         ("C-c s K" . activities-discard))
+  :init
+  (desktop-save-mode)
+  (activities-mode)
+  (activities-tabs-mode))
+
+(use-package bufferlo
+  :straight t
+  :bind (("C-x k" . bufferlo-remove))
+  :config
+  (defvar my-consult--source-buffer
+    `(:name "All Buffers"
+            :narrow   ?a
+            :hidden   t
+            :category buffer
+            :face     consult-buffer
+            :history  buffer-name-history
+            :state    ,#'consult--buffer-state
+            :items ,(lambda () (consult--buffer-query
+                           :sort 'visibility
+                           :as #'buffer-name)))
+    "All buffer candidate source for `consult-buffer'.")
+
+  (defvar my-consult--source-local-buffer
+    `(:name nil
+            :narrow   ?b
+            :category buffer
+            :face     consult-buffer
+            :history  buffer-name-history
+            :state    ,#'consult--buffer-state
+            :default  t
+            :items ,(lambda () (consult--buffer-query
+                           :predicate #'bufferlo-local-buffer-p
+                           :sort 'visibility
+                           :as #'buffer-name)))
+    "Local buffer candidate source for `consult-buffer'.")
+
+  (setq consult-buffer-sources '(consult--source-hidden-buffer
+                                 my-consult--source-buffer
+                                 my-consult--source-local-buffer))
+
+  :init
+  (bufferlo-mode 1)
+  (tab-line-mode nil))
 
 (provide 'my-tabspaces)
